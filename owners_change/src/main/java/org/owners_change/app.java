4bd50495520bb6bc49b44ed127319e4409c99e84
Juan Perea <jpereac@gmail.com>
2019-01-16 17:06:31 +0100

    upload files

---
+++ owners_change/src/main/java/org/owners_change/app.java
@@ -0,0 +1,13 @@
+package org.Owners_Change;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ sanctions_payment/src/main/java/org/sanctions_payment/app.java
@@ -0,0 +1,13 @@
+package org.Sanctions_Payment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
